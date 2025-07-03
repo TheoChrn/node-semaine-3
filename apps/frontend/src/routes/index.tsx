c5ae0b60a5c7117375d85eed51b14fe95e719a37
TheoChrn <theo.chrn@gmail.com>
2025-07-03 15:27:26 +0200

    ui fix

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -18,46 +18,44 @@ function App() {
   return (
     <Ariakit.HeadingLevel>
       <div className="p-4 ak-layer">
-        <Wrapper className="space-y-6">
-          <Ariakit.Heading className="text-2xl mb-4">
-            Furnitures list
-          </Ariakit.Heading>
-
+        <Wrapper className="flex justify-between gap-8">
           {furnitures ? (
             <>
               {Object.entries(furnitures ?? []).map(([key, value]) => (
-                <Ariakit.HeadingLevel key={key}>
-                  <Ariakit.Heading className="text-2xl font-semibold capitalize">
-                    {`${key}s`}
-                  </Ariakit.Heading>
-                  {value.map((furniture) => (
-                    <Ariakit.HeadingLevel>
-                      <article
-                        key={furniture.id}
-                        className="flex border ak-frame p-3 gap-8"
-                      >
-                        <div className="h-20 aspect-square ak-layer-contrast" />
-                        <div className="my-auto space-y-2">
-                          <Ariakit.Heading className="text-2xl font-semibold">
-                            {furniture.value}
-                          </Ariakit.Heading>
-                          <ul className="flex flex-wrap gap-x-1 gap-y-2">
-                            {Object.values(furniture.materials).map(
-                              ({ materials }) =>
-                                materials.map((m) => (
-                                  <li key={m.id}>
-                                    <span className="ak-frame ak-layer-pop py-1 px-3 capitalize">
-                                      {m.value}
-                                    </span>
-                                  </li>
-                                ))
-                            )}
-                          </ul>
-                        </div>
-                      </article>
-                    </Ariakit.HeadingLevel>
-                  ))}
-                </Ariakit.HeadingLevel>
+                <div className="flex-1 space-y-4">
+                  <Ariakit.HeadingLevel key={key}>
+                    <Ariakit.Heading className="text-2xl font-semibold capitalize">
+                      {`${key}s`}
+                    </Ariakit.Heading>
+                    {value.map((furniture) => (
+                      <Ariakit.HeadingLevel>
+                        <article
+                          key={furniture.id}
+                          className="flex border ak-frame p-3 gap-4"
+                        >
+                          <div className="h-20 aspect-square ak-layer-contrast" />
+                          <div className="my-auto space-y-2">
+                            <Ariakit.Heading className="text-2xl font-semibold">
+                              {furniture.value}
+                            </Ariakit.Heading>
+                            <ul className="flex flex-wrap gap-x-1 gap-y-2">
+                              {Object.values(furniture.materials).map(
+                                ({ materials }) =>
+                                  materials.map((m) => (
+                                    <li key={m.id}>
+                                      <span className="ak-frame ak-layer-pop py-1 px-3 capitalize">
+                                        {m.value}
+                                      </span>
+                                    </li>
+                                  ))
+                              )}
+                            </ul>
+                          </div>
+                        </article>
+                      </Ariakit.HeadingLevel>
+                    ))}
+                  </Ariakit.HeadingLevel>
+                </div>
               ))}
             </>
           ) : (
